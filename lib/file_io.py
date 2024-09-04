a86597487cc88bd871d1ea2bdf671facb296ed0e
emmanuel ronoh <eronoh036@gmail.com>
2024-09-04 14:27:24 +0300

    first commit

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,15 @@
-def write_file(file_name, file_content):
-    pass
+def write_file(file_path, content):
+    """Write content to a file, creating the file if it does not exist."""
+    with open(f'{file_path}.txt', 'w') as f:
+        f.write(content)
 
-def append_file(file_name, append_content):
-    pass
+def append_file(file_path, content):
+    """Append content to a file, creating the file if it does not exist."""
+    with open(f'{file_path}.txt', 'a') as f:
+        f.write(content)
 
-def read_file(file_name):
-    pass
+def read_file(file_path):
+    """Read content from a file."""
+    with open(f'{file_path}.txt', 'r') as f:
+        return f.read()
+
